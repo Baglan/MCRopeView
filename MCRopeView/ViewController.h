60bd1493f4364a01b483400aadf0d2764a8cce74
Baglan Dosmagambetov <baglan.dos@gmail.com>
2013-03-15 18:52:58 +0600

    Initial Commit

---
+++ MCRopeView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MCRopeView
+//
+//  Created by Baglan on 3/15/13.
+//  Copyright (c) 2013 MobileCreators. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
